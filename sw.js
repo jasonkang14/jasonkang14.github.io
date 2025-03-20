d7191e8488ad68cc14c9d7f9086134dec0462d63
Jason Kang <jasonkang14@gmail.com>
2025-03-20 12:34:23 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "styles.3c91aee1fa0e12b154a1.css"
   },
   {
-    "url": "app-20c6c3b025110856910f.js"
+    "url": "app-79ad3e073410a659da28.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f33cac42a163bf95db4492b4a55386a7"
+    "revision": "fbfad0555c2d47c380c0d65c00a0dc4e"
   },
   {
     "url": "manifest.webmanifest",
@@ -154,7 +154,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-20c6c3b025110856910f.js`))) {
+  if (!resources || !(await caches.match(`/app-79ad3e073410a659da28.js`))) {
     return await fetch(event.request)
   }
 
